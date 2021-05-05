8ccc9d5f76bca7579fc9bcf61e93006dc421517e
Rafał Zaborowski <raffik2000@gmail.com>
2021-05-05 15:10:49 +0200

    Creating basic models

---
+++ Evento.Core/Domain/Ticket.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Evento.Core.Domain
+{
+    class Ticket : Entity
+    {
+        public Guid EventId { get; protected set; }
+        public int Seating { get; protected set; }
+        public decimal Price { get; protected set; }
+        public Guid? UserId { get; protected set; }
+        public string Username { get; protected set; }
+        public DateTime? PurchasedAt { get; protected set; }
+        public bool Pruchased => UserId.HasValue;
+
+        protected Ticket()
+        {
+
+        }
+        public Ticket(Event @event, int seating, decimal price)
+        {
+            EventId = @event.Id;
+            Seating = seating;
+            Price = price;
+        }
+    }
+}
