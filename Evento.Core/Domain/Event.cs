050a3b04b362c3b7546fd17ae2c321d20cd9cdc6
Rafał Zaborowski <raffik2000@gmail.com>
2021-05-12 15:50:53 +0200

    expansion of the tickets domain model

--- Evento.Core/Domain/Event.cs
+++ Evento.Core/Domain/Event.cs
@@ -15,8 +15,8 @@ namespace Evento.Core.Domain
         public DateTime EndDate { get; protected set; }
         public DateTime UpdateAt { get; protected set; }
         public IEnumerable<Ticket> Tickets => _tickets;
-        public IEnumerable<Ticket> PurshasedTickets => Tickets.Where(x => x.Pruchased);
-        public IEnumerable<Ticket> AvailableTickets => Tickets.Except(PurshasedTickets);
+        public IEnumerable<Ticket> PurchasedTickets => Tickets.Where(x => x.Purchased);
+        public IEnumerable<Ticket> AvailableTickets => Tickets.Except(PurchasedTickets);
 
 
         protected Event()
@@ -59,5 +59,32 @@ namespace Evento.Core.Domain
                 seating++;
             }
         }
+        public void PurchaseTickets(User user, int amount)
+        {
+            if (AvailableTickets.Count() < amount)
+            {
+                throw new Exception($"Not enough available tickets to purchase ({amount}) by user: '{user.Name}'.");
+            }
+            var tickets = AvailableTickets.Take(amount);
+            foreach (var ticket in tickets)
+            {
+                ticket.Purchase(user);
+            }
+        }
+
+        public void CancelPurchasedTickets(User user, int amount)
+        {
+            var tickets = GetTicketsPurchasedByUser(user);
+            if (tickets.Count() < amount)
+            {
+                throw new Exception($"Not enough purchased tickets to be canceled ({amount}) by user: '{user.Name}'.");
+            }
+            foreach (var ticket in tickets.Take(amount))
+            {
+                ticket.Cancel();
+            }
+        }
+        public IEnumerable<Ticket> GetTicketsPurchasedByUser(User user)
+            => PurchasedTickets.Where(x => x.UserId == user.Id);
     }
 }
--- Evento.Core/Domain/Ticket.cs
+++ Evento.Core/Domain/Ticket.cs
@@ -12,7 +12,7 @@ namespace Evento.Core.Domain
         public Guid? UserId { get; protected set; }
         public string Username { get; protected set; }
         public DateTime? PurchasedAt { get; protected set; }
-        public bool Pruchased => UserId.HasValue;
+        public bool Purchased => UserId.HasValue;
 
         protected Ticket()
         {
@@ -23,6 +23,27 @@ namespace Evento.Core.Domain
             EventId = @event.Id;
             Seating = seating;
             Price = price;
+        }
+        public void Purchase(User user)
+        {
+            if (Purchased)
+            {
+                throw new Exception($"Ticket was already purchased by user: '{Username}' at: {PurchasedAt}'.");
+            }
+            UserId = user.Id;
+            Username = user.Name;
+            PurchasedAt = DateTime.UtcNow;
+        }
+
+        public void Cancel()
+        {
+            if (!Purchased)
+            {
+                throw new Exception($"Ticket was not purchased and can not be canceled.");
+            }
+            UserId = null;
+            Username = null;
+            PurchasedAt = null;
         }
     }
 }
